3082064aa3c01d978f55bf19b44c1db26781538f
Dominic Tarr <dominic.tarr@gmail.com>
2013-07-22 10:18:53 +0100

    use tape

--- test.js
+++ test.js
@@ -1,44 +1,46 @@
-
-
 var mps = require('./index')
 
 var encode = mps.createEncodeStream()
 var decode = mps.createDecodeStream()
 var assert = require('assert')
 
-var expected = [
-  1,
-  0,
-  null,
-  true,
-  "hello",
-  'string\nwith\nlines',
-  {object: true, name: 'no need for a name'},
-  ['this', 'that', {}, [], [null]],
-  Math.PI,
-  true,
-  false,
-//  Infinity,   //doesn't work
-//  NaN,        //doesn't work
-  {obj: {}}
-]
+require('tape')('simple', function (t) {
 
-var toSend = expected.slice()
-  , expectedItems = expected.slice()
-  , actual = []
+  var expected = [
+    1,
+    0,
+    null,
+    true,
+    "hello",
+    'string\nwith\nlines',
+    {object: true, name: 'no need for a name'},
+    ['this', 'that', {}, [], [null]],
+    Math.PI,
+    true,
+    false,
+  //  Infinity,   //doesn't work
+  //  NaN,        //doesn't work
+    {obj: {}}
+  ]
 
-encode
-.pipe(decode)
-.on('data', function (obj) {
-  console.log(obj)
-  actual.push(obj)
-  assert.deepEqual(obj, expectedItems.shift())
+  var toSend = expected.slice()
+    , expectedItems = expected.slice()
+    , actual = []
+
+  encode
+  .pipe(decode)
+  .on('data', function (obj) {
+    console.log(obj)
+    actual.push(obj)
+    t.deepEqual(obj, expectedItems.shift())
+  })
+  .on('end', function () {
+    t.deepEqual(actual, expected)
+    t.end()
+  })
+
+  while(toSend.length)
+    encode.write(toSend.shift())
+  encode.end()
+
 })
-.on('end', function () {
-  assert.deepEqual(actual, expected)
-  console.log('passed')
-})
-
-while(toSend.length)
-  encode.write(toSend.shift())
-encode.end()
